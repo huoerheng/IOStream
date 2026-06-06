63d2f81a46884b02d586d4ff608380ccdc47d798
sleepyhead <3062846914@qq.com>
2026-06-06 20:24:51 +0800

    读写文件基本操作

--- src/Input_Output/IO.java
+++ src/Input_Output/IO.java
@@ -1,5 +1,6 @@
 package Input_Output;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -8,12 +9,35 @@ public class IO {
         System.out.println("当前工作目录：" + System.getProperty("user.dir"));
         //路径 / File对象都可以，但是父级路径必须存在，如果文件已存在就会清空内容,同一个new对象会继续write
         //默认关闭，开启续写true
-        FileOutputStream fos = new FileOutputStream("file.txt",true);
+      /*  FileOutputStream fos = new FileOutputStream("file.txt",true);
         //write方法的参数是数字，都是对应ASCII的字符
         String str = "hello world \r\n666";
         byte[] bytes = str.getBytes();
         fos.write(bytes);
         fos.write(bytes,1,3);
+        fos.close();*/
+
+        /*FileInputStream fi = new FileInputStream("file.txt");
+        int b;
+        while ((b = fi.read()) != -1) {
+            System.out.print((char) b);
+        }
+        fi.close();*/
+
+
+
+        long l = System.currentTimeMillis();
+        FileOutputStream fos = new FileOutputStream("c.txt");
+        FileInputStream fis = new FileInputStream("file.txt");
+        int b;
+        while ((b=fis.read()) != -1){
+            fos.write(b);
+        }
+
         fos.close();
+        fis.close();
+        long l2 = System.currentTimeMillis();
+        System.out.println(l2-l);
+
     }
 }
